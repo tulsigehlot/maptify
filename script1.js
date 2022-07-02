2a144f7d5e98ffe1d74735208765f0cbb3277a9f
Tulsi Gehlot <tulsigehlot6@gmail.com>
2022-07-03 01:20:06 +0530

    added the functionality of managing the workout data and creating classes

--- script1.js
+++ script1.js
@@ -2,6 +2,44 @@
 
 //prettier ignore
 //const months =['january','febuary','march','april','may','june','july','august','september','october','november','december'];
+class Workout {
+  date = new Date();
+  id = (Date.now() + "").slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords; //[lat,lng]
+    this.distance = distance; //in km
+    this.duration = duration; // in min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    // min/km
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    // km/h
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
 
 ///////////////////////////////////////
 // APPLICATION ARCHITECTURE
