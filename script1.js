9bfced1b1fce19a2ca8d5a14dcf95ed1ec78a8d5
Tulsi Gehlot <tulsigehlot6@gmail.com>
2022-07-02 20:52:01 +0530

    added displaying marker functionality

--- script1.js
+++ script1.js
@@ -23,6 +23,9 @@ if(navigator.geolocation)
                 const {longitude} = position.coords;
                 console.log(`${latitude},${longitude}`);
                 
+                ///////////////////////////////////////
+                // adding leaflet library for displaying map  
+
                 const coords = [latitude,longitude];
                 const map = L.map('map').setView(coords, 13);
 
@@ -30,10 +33,26 @@ if(navigator.geolocation)
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 }).addTo(map);
 
-                L.marker(coords).addTo(map)
-                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-                .openPopup();
+                map.on('click', function (mapEvent){
+                    console.log(mapEvent);
+                    const {lat,lng} = mapEvent.latlng;
 
+                    L.marker([lat,lng])
+                    .addTo(map)
+                   .bindPopup(L.popup({
+                        maxWidth: 250,
+                        minWidth: 100,
+                        autoClose: false,
+                        closeOnClick: false,
+                        className: 'running-popup',
+                   })
+                 )
+                .setPopupContent('Workout')
+                   .openPopup();
+
+                });
+
+                
             },
             function(){
                 alert('could not get your location');
